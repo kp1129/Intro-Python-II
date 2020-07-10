911a0c7eff74f411202098d39a76843352c05921
kp1129 <katya.p.1129@gmail.com>
2020-07-10 11:32:01 -0700

    module 2 - more functionalities

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,27 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons."),
+                     "North of you, the cave mount beckons.", None),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", Item("map", "Migrating Moon Map is infused with powerful magic that can lead you to whatever it is that your heart desires...no matter where in the universe it is.")),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", Item("sword",
+                "The magical Elven Sword makes its bearer impossible to defeat.")),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", Item("scroll", "Ancient Scroll of Mysteries written by the exiled elven mages in the world before.")),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", None),
 }
 
 
@@ -33,6 +35,15 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+
+# declare all the items
+
+# item = {
+#     'sword': Item("Elven Sword",
+#                 "A magical elven sword that makes its bearer impossible to defeat"),
+#     'scroll': Item("Scroll of Mysteries", "An ancient scroll written by the exiled elven mages in the world before"),
+#     'map': Item("Migrating Moon Map", "A map infused with powerful magic that can lead you to whatever it is that your heart desires...no matter where in the universe it is")            
+# }
 
 #
 # Main
@@ -61,16 +72,31 @@ print(f'{new_player.name}, you are standing in front of the {current_room.name}.
 
 while is_playing:
 
-    user_choice = input('Where would you like to go from here? n/s/e/w: ')
+    user_choice = input('Where would you like to go from here? [n] [s] [e] [w]: ')
 
     if user_choice == 'q':
         print('Thanks for playing!')
         is_playing = False
+
+    elif user_choice == 'get':
+        if len(current_room.items) > 0:
+            new_player.items.append(current_room.items[0])
+            del current_room.items[0]
+            new_item = new_player.items[-1]
+            print(f"You picked up a {new_item}. Use it wisely.")
+            # for item in current_room.items:
+            #     print(item.name + ": " + item.description)
+
     elif user_choice == 'n':
         if type(current_room.n_to) is Room:
                 current_room = current_room.n_to
                 new_player.current_room = current_room.name
                 print(f'You are now in the {current_room.name}. {current_room.description}')
+                if len(current_room.items) > 0 and current_room.items[0] != None:
+                    print("This room contains the following items: ")
+                    for item in current_room.items:
+                        print(f"Item: {item.name}: {item.description}")
+                    print("To pick up an item, enter [get ITEM]")    
         else:
             print('Sorry, you cannot go there. Pick another direction.')        
     elif user_choice == 's':
@@ -78,6 +104,10 @@ while is_playing:
                 current_room = current_room.s_to
                 new_player.current_room = current_room.name
                 print(f'You are now in the {current_room.name}. {current_room.description}')
+                if len(current_room.items) > 0 and current_room.items[0] != None:
+                    print("This room contains the following items: ")
+                    for item in current_room.items:
+                        print(item)
         else:
             print('Sorry, you cannot go there. Pick another direction.') 
     elif user_choice == 'e':
@@ -85,6 +115,10 @@ while is_playing:
                 current_room = current_room.e_to
                 new_player.current_room = current_room.name
                 print(f'You are now in the {current_room.name}. {current_room.description}')
+                if len(current_room.items) > 0 and current_room.items[0] != None:
+                    print("This room contains the following items: ")
+                    for item in current_room.items:
+                        print(item)
         else:
             print('Sorry, you cannot go there. Pick another direction.')                   
     elif user_choice == 'w':
@@ -92,5 +126,9 @@ while is_playing:
                 current_room = current_room.w_to
                 new_player.current_room = current_room.name
                 print(f'You are now in the {current_room.name}. {current_room.description}')
+                if len(current_room.items) > 0 and current_room.items[0] != None:
+                    print("This room contains the following items: ")
+                    for item in current_room.items:
+                        print(item)
         else:
             print('Sorry, you cannot go there. Pick another direction.')         
---
+++ src/item.py
@@ -0,0 +1,8 @@
+# class to create items
+
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    def __str__(self):
+        return self.name + ". " + self.description    
